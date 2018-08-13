1c7e66fbd4e58f691a38deb2a5d0104b9b2cb092
Yusuf Kelo <ykelo@gulfnews.com>
2018-08-13 09:50:56 +0400

    update

---
+++ lib/pages/product.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ProductPage extends StatelessWidget {
+
+   @override
+   Widget build(BuildContext context) {
+       return Scaffold(
+          appBar: AppBar(
+            title: Text('Product Details')
+          ),
+         body: Column(
+             children: <Widget>[
+               Center(child: Text('Product details one')),
+               RaisedButton(
+                 child: Text('Back'),
+                 onPressed: () =>  Navigator.pop(context)
+               )
+             ]
+         )
+       );
+   }
+}
--- lib/products.dart
+++ lib/products.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './pages/product.dart';
 
 class Products extends StatelessWidget {
   final List<String> products;
@@ -7,12 +8,26 @@ class Products extends StatelessWidget {
     print('[Products Widget] Constructor');
   }
 
+
+
+
   Widget _buildProductItem(BuildContext context, int index) {
      return Card(
                       child: Column(
                         children: <Widget>[
                           Image.asset('assets/food.jpg'),
-                          Text(products[index])
+                          Text(products[index]),
+                          ButtonBar(
+                              alignment: MainAxisAlignment.center,
+                              children: <Widget>[FlatButton(
+                             child: Text('Details'),
+                             onPressed:() => Navigator.push(
+                               context,
+                               MaterialPageRoute(
+                                 builder: (BuildContext context) =>ProductPage(),
+                               ),
+                             ),
+                          )])
                         ],
                       ),
                     );
